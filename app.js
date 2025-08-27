06d6f9905d5ebfa5e8a7b48fd71b04f378b7ac30
hamidur800 <hamidur800t@gmail.com>
2025-08-27 21:20:57 +0600

    call_btn-and-call_histry_clear-functions-fainaly-done

--- app.js
+++ app.js
@@ -33,3 +33,79 @@ document.getElementById("heart_plus-8").addEventListener("click", function () {
 document.getElementById("heart_plus-9").addEventListener("click", function () {
   heartCountValue("heart_plus-9");
 });
+
+let coin = 100;
+
+let callHistry = []; // বাইরে ডিফাইন করো, না হলে প্রতিবার খালি হয়ে যাবে
+
+function makeCall(serviceName, number) {
+  if (coin === 0) {
+    alert("কল করার জন্য পর্যাপ্ত পরিমাণ কয়েন নাই");
+    return;
+  }
+
+  alert(`Calling... ${serviceName} Number : ${number}`);
+
+  coin -= 20;
+  if (coin < 0) coin = 0;
+  document.getElementById("coin").innerText = " " + coin;
+
+  const data = {
+    name: serviceName,
+    number: number,
+    date: new Date().toLocaleTimeString(),
+  };
+  callHistry.push(data);
+
+  const callHistryContainar = document.getElementById("call-histry-containar");
+  const div = document.createElement("div");
+  div.innerHTML = `
+   <div class="bg-[#FAFAFA] mt-3 px-5 py-7 rounded-xl mx-1.5">
+        <div class="call_history">
+            <div class="flex justify-between items-center">
+                <div class="flex flex-col">
+                    <div class="call_title">${data.name}</div>
+                    <div class="call_description">${data.number}</div>
+                </div>
+                <div class="call_time">${data.date}</div>
+            </div>
+        </div>
+    </div>
+  `;
+  callHistryContainar.appendChild(div);
+}
+
+document.getElementById("callBtn-1").addEventListener("click", function () {
+  makeCall("জাতীয় জরুরি সেবা", "999");
+});
+
+document.getElementById("callBtn-2").addEventListener("click", function () {
+  makeCall("পুলিশ", "999");
+});
+document.getElementById("callBtn-3").addEventListener("click", function () {
+  makeCall("ফায়ার সার্ভিস", "999");
+});
+document.getElementById("callBtn-4").addEventListener("click", function () {
+  makeCall("অ্যাম্বুলেন্স", "1994-999999");
+});
+document.getElementById("callBtn-5").addEventListener("click", function () {
+  makeCall("নারী ও শিশু সহায়তা", "109");
+});
+document.getElementById("callBtn-6").addEventListener("click", function () {
+  makeCall("দুদক", "106");
+});
+document.getElementById("callBtn-7").addEventListener("click", function () {
+  makeCall("বিদ্যুৎ বিভ্রাট", "16216");
+});
+document.getElementById("callBtn-8").addEventListener("click", function () {
+  makeCall("ব্র্যাক", "16445");
+});
+document.getElementById("callBtn-9").addEventListener("click", function () {
+  makeCall("বাংলাদেশ রেলওয়ে", "163");
+});
+
+document.getElementById("clear-histry").addEventListener("click", function () {
+  callHistry = [];
+  document.getElementById("call-histry-containar").innerText = "";
+  alert("কল হিস্ট্রি ক্লিয়ার করা হয়েছে!");
+});
